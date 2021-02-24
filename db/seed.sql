f08a733b97a49b739a5655ac712de2a9d36f6c3c
Nick Pasch <paschnick100@gmail.com>
2021-02-23 23:01:04 -0700

    refactoring git project

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name, devoured) VALUES ('Double Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('De Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chipotle Burger', true);
+INSERT INTO burgers (name) VALUES ('Jalepeno demon burger');
